a116936ff2316b78e43858b76dbfefee060a0fef
Dmitriy Sosipatrov <losox@yandex.ru>
2021-09-26 22:55:16 +0300

    Исправления и улучшения по заданию

--- js/main.js
+++ js/main.js
@@ -3,21 +3,24 @@
 Источник данных:
 https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 */
-const getWrongRange = (numberOne, numberTwo) => {
-  if (numberTwo <= numberOne || numberOne < 0 ) {
-    throw  new RangeError('Диапазон выбран не правильно');
+const isIncorrectRange = (min, max) => {
+  if (max <= min || min < 0) {
+    return true;
   }
+  // return (max <= min || min < 0) ? true : false;
 };
 
+
 const getRandomNumberFromRange = (from, to) => {
-  getWrongRange(from, to);
+  if (isIncorrectRange(from, to)) {
+    throw  new RangeError('Диапазон выбран не правильно');
+  }
   from = Math.ceil(from);
   to = Math.floor(to);
   return Math.floor(Math.random() * (to - from + 1)) + from;
 };
 
-
-getRandomNumberFromRange(10, 50);
+getRandomNumberFromRange(0, 10);
 
 /*
 Случайное целое число c плавающей точкой из диапазона(включая от и до) положительных чисел с указанным кол-ом знаков после запятой.
@@ -28,12 +31,13 @@ https://efim360.ru/javascript-kak-proverit-chislo-na-czeloe
 */
 
 const getRandomNumberWithDotFromRange = (from, to, afterDot) => {
-  getWrongRange(from, to);
-  let number = (Math.random() * ( to - from)) + from;
-  if((from % 1 === 0) || (to % 1 === 0)){
-    number = (Math.random() * (to - from + 1)) + from;
+  if (isIncorrectRange(from, to)) {
+    throw new RangeError('Диапазон выбран не правильно');
   }
+  const num = ((from % 1 === 0) || (to % 1 === 0)) ? 1 : 0;
+  const number = (Math.random() * (to - from + num)) + from;
+
   return Number(number.toFixed(afterDot));
 };
 
-getRandomNumberWithDotFromRange(1.6, 1.8, 3);
+getRandomNumberWithDotFromRange(10, 30, 5);
